847b0c74b654c6816873441a7e12df6e9cd6cd8c
Daniele Alessandri <suppakilla@gmail.com>
2008-12-03 12:13:09 +0100

    Just like CouchDB's svn trunk r720389: Log the offending doc._id when the view function raises an exception.

--- viewserver.lua
+++ viewserver.lua
@@ -63,7 +63,7 @@ local function exec(fun, ...)
             if result.error.id == 'fatal_error' then
                 error(cdb_error('map_runtime_error', 'function raised fatal exception'))
             else
-                log('function raised exception (' .. result.error.reason .. ')')
+                log('function raised exception (' .. result.error.reason .. ') with doc._id ' .. doc._id)
             end
         else
             log('function raised exception (' .. (result or 'unknown') .. ')')
